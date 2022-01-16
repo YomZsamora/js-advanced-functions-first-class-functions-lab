89f3c9a0e18888cbf1e19d66690d11cea261e296
yomZsamora <samora.y@adzumi.co.ke>
2022-01-16 19:25:28 +0300

    Returns a new array containing the first two drivers from the passed-in array.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Code your solution in this file!
+// Returns a new array containing the first two drivers from the passed-in array
+let returnFirstTwoDrivers = drivers => { return drivers.slice(0, 2); }
+
+// Returns a new array containing the first two drivers from the passed-in array
+let returnLastTwoDrivers = drivers => { return drivers.slice(0, 2); }
