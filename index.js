5f0979406cfbf78a6fa6535fc7b4b4c77e958593
yomZsamora <samora.y@adzumi.co.ke>
2022-01-17 08:40:26 +0300

    fareDoubler() is a function that doubles fare.

--- index.js
+++ index.js
@@ -11,14 +11,11 @@ let selectingDrivers = [
 ];
 
 // createFareMultiplier()returns a function that should multiply a given value using the created multiplier
-function createFareMultiplier(multiplier) {
+let createFareMultiplier = multiplier => {
     return function(fare) {
         return multiplier * fare;
     }
 }
 
-// Declared a variable with const and assign a function returned by createFareMultiplier() to it.
-const fare = createFareMultiplier();
-
-// fareDoubler() is a function that doubles fare
-let fareDoubler = fare => fare * 2
+// Declared a variable with const and assign a function returned by createFareMultiplier() to it. fareDoubler() is a function that doubles fare
+const fareDoubler = createFareMultiplier(2);
