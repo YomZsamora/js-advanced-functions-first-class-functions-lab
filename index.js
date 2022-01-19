39f0700bd95c5bc67a3a3bd15e35f85550b5a07e
yomZsamora <samora.y@adzumi.co.ke>
2022-01-19 17:09:13 +0300

    Completed Lab.

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const returnLastTwoDrivers = drivers => { return drivers.slice(-2); }
 
 // selectingDrivers has the `returnFirstTwoDrivers` function to as its first element and `returnLastTwoDrivers` function to as its last element
 let selectingDrivers = [
-    returnFirstTwoDrivers, 
-    returnLastTwoDrivers
+	returnFirstTwoDrivers, 
+	returnLastTwoDrivers
 ];
 
 // createFareMultiplier()returns a function that should multiply a given value using the created multiplier
 let createFareMultiplier = multiplier => {
-    return function(fare) {
-        return multiplier * fare;
-    }
+	return function(fare) {
+		return multiplier * fare;
+	}
 }
 
 // Declared a variable with const and assign a function returned by createFareMultiplier() to it. fareDoubler() is a function that doubles fare
@@ -26,5 +26,5 @@ const fareTripler = createFareMultiplier(3);
 /* This function takes two arguments, an array of drivers and either the returnFirstTwoDrivers() or returnLastTwoDrivers() function. 
 Based on these two arguments, selectDifferentDrivers() will return either the first two drivers or the last two drivers. */
 let selectDifferentDrivers = (arrayOfDrivers, selectedDrivers) => {
-    return selectedDrivers(arrayOfDrivers)
+	return selectedDrivers(arrayOfDrivers)
 }
